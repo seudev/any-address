209ae2b50a07b6959afd671b0b030b6dcabdc205
Thomás Sousa Silva (ThomasSousa96) <thomas.sousa.96@gmail.com>
2018-12-03 02:47:59 -0300

    Add security with overjax-security-keycloak

--- src/main/java/com/seudev/anyaddress/app/controller/CountryController.java
+++ src/main/java/com/seudev/anyaddress/app/controller/CountryController.java
@@ -22,49 +22,53 @@ import javax.ws.rs.core.UriInfo;
 import com.seudev.anyaddress.model.Country;
 import com.seudev.anyaddress.query.filter.CountryFilter;
 import com.seudev.anyaddress.service.CountryService;
+import com.seudev.overjax.annotation.Authenticated;
+import com.seudev.overjax.annotation.Authorize;
 import com.seudev.overjax.annotation.Controller;
 import com.seudev.overjax.annotation.WrapResponse;
 
 @Controller
 @WrapResponse
+@Authenticated
 @Path("/api/v1/country")
 @Produces(APPLICATION_JSON)
 @Consumes(APPLICATION_JSON)
 public class CountryController {
-    
+
     @Context
     private UriInfo uriInfo;
-    
+
     @EJB
     private CountryService countryService;
-    
+
     @GET
     @Path("{id}")
+    @Authorize("$.hasRole('GET_COUNTRY')")
     public Response find(UUID id) {
         Country country = countryService.findByID(id);
         return Response.status(country == null ? NOT_FOUND : OK).build();
     }
-    
+
     @GET
     public Response findAll(@BeanParam CountryFilter filter) {
         List<Country> list = countryService.findAll(filter);
         return Response.ok(list).build();
     }
-    
+
     @PUT
     @Path("{id}")
     public Response merge(Country country) {
         countryService.merge(country);
         return Response.ok(country).build();
     }
-    
+
     @POST
     public Response persist(Country country) {
         countryService.persist(country);
-        
+
         return Response.created(uriInfo.getAbsolutePathBuilder().path(country.getId().toString()).build())
                 .entity(country)
                 .build();
     }
-    
+
 }
